3884525380e0486da3b5e727a3b858685ddd53f9
Limbeck Engineering LLC <limbeck.engineering@gmail.com>
2014-04-21 21:36:13 -0400

    Update stepper.py

--- stepper/stepper.py
+++ stepper/stepper.py
@@ -1,12 +1,19 @@
-import Adafruit_BBIO.GPIO as GPIO
+#example Python code used to run stepper motors
+
 from BBstepper import Stepper
 import config
 
 
 stepper = Stepper()
-
 stepper.init_pins(config.pins)	
 
+
+
 stepper.spin_clockwise(config.pins, 3, 30)
+
+stepper.spin_counterclockwise(config.pins, 3, 30)
+
+
+
 
 stepper.cleanup(pins)
