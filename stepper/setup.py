5af24b5c63b808f25b7f05f96a8ed710e38f2775
Limbeck Engineering LLC <limbeck.engineering@gmail.com>
2014-04-21 21:30:20 -0400

    Update setup.py

--- stepper/setup.py
+++ stepper/setup.py
@@ -7,16 +7,18 @@ setup(
 	name='BBstepper',
 	version='0.1',
 	author='Travis Libsack',
+	url='https://github.com/limbeckengineering/BBB/stepper.git',
 	modules=['BBstepper'],
 	scripts=[],
-	description='Moduke to control steppers using the Beagle Bone Black and Adafruits library'
+	description='Module to control stepper motors using the Beagle Bone Black and Adafruits BBIO GPIO library'
 )
 
 setup(
 	name='config',
 	version='0.1',
 	author='Travis Libsack',
+	url='https://github.com/limbeckengineering/BBB/stepper.git',
 	modules=['config'],
 	scripts=[],
-	description='Global variables for stepper motors'
+	description='Global variables for the Project RoboGoby float on the BeagleBone Black.'
 )
