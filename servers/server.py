880c8a7143d170a77fd9a4312307d460720a2186
Limbeck Engineering LLC <limbeck.engineering@gmail.com>
2014-04-23 13:50:02 -0400

    Update server.py

--- servers/server.py
+++ servers/server.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
 import socket
+import time
+import config
+
+
+data = {"temp": "67.45", "thruster 1": "5"} 
 
 s = socket.socket()
 host = socket.gethostname()
@@ -13,4 +18,4 @@ c, addr = s.accept()
 
 print 'Connection from: ', addr
 
-c.send('Hello this is the BeagleBone black')
+c.send(data)
