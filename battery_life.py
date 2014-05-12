ec458c1607d8951b87abff8bc8dccf81f4dfe8e2
Limbeck Engineering LLC <limbeck.engineering@gmail.com>
2014-05-12 08:33:56 -0400

    Update battery_life.py

--- battery_life.py
+++ battery_life.py
@@ -2,5 +2,21 @@
 
 import time 
 import config
+import Adafruit_BBIO.ADC as ADC
 
 
+
+class Battery_Life(object):
+  
+  def init():
+    ADC.setup()
+    
+  def read_voltage():
+    while True:
+        value = ADC.read(config.battery_pin)
+       voltage = value*1.8
+       return voltage
+       time.sleep(5)
+       
+       
+      
