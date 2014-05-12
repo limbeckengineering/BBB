433f02975fa3b36a04db99305c1cf22950d450af
Limbeck Engineering LLC <limbeck.engineering@gmail.com>
2014-05-11 22:43:37 -0400

    Update spool.py

--- spool.py
+++ spool.py
@@ -1,2 +1,25 @@
 #!/bin/env/python
 
+import sys
+import time
+import config
+from BBStepper import Stepper
+
+
+def spool(depth1, depth2):
+  rotations = depth/1.2
+  rpm = (depth2-depth1)/1
+  
+def unspool(depth):
+    rotations = depth/1.2
+    rpm = (depth2 - depth1)/1
+    
+
+class Spool(object):
+    def init(self):
+        stepper1 = Stepper.init(config.stepper1)
+        stepper2 = Stepper.init(config.stepper2)
+        stepper1.init()
+        stepper2.init()
+    
+    def read
