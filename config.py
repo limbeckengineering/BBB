c1f3e0f04df23946628086b86cc093f878c51b9a
Limbeck Engineering LLC <limbeck.engineering@gmail.com>
2014-05-13 13:42:33 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 #!/bin/env/python
 
-host = "10.0.1.202"
-port = 2345
+HOST = "10.0.1.202"
+OCUPort = 2345
+SUBPort = 1234
+
 stepper1 = {"clk":"P8_17","dir":"P8_15"}
 
 stepper2 = {"clk":"P8_16", "dir":"P8_18"}
