1c0b132fc5c4fa3a0290d16bb39123af71160951
limbeckengineering <limbeck.engineering@gmail.com>
2014-05-19 01:15:01 +0000

    fixing temperature file

--- DS18B20.py
+++ DS18B20.py
@@ -1,9 +1,26 @@
 import time
+import config
+import os
 
 w1 = "/sys/bus/w1/devices/28-00000494acf0/w1_slave"
 
-while True:
-	raw = open(w1, "r").read()
-	print "Temp: "+str((float(raw.split("t=")[-1])/1000)*(9.0/5.0) + 32.0)+" F"
-	time.sleep(1)
+def init():
+	os.system("echo BB-W1:00A0 > /sys/devices/bone_capemgr.9/slots")	
 
+def readTemp():
+	temp = ""
+	raw = open(w1, "r").read()
+	f = str((float(raw.split("t=")[-1])/1000)*(9.0/5.0) + 32.0)
+	c = str(float(raw.split("t=")[-1])/1000)
+	temp += f
+	temp += ","
+	temp += c
+	return temp
+
+if __name__ == "__main__":
+	init()
+	while 1:
+		value = readTemp()
+		fahrenheit,celcius = value.split(",")
+		print fahrenheit +" " + celcius
+		time.sleep(3)
