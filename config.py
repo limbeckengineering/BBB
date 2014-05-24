e30bd2eb0b1e929c86f63f1e160774d0e2647391
limbeckengineering <limbeck.engineering@gmail.com>
2014-05-24 22:19:36 +0000

    fixed files

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/bin/env/python
+HOST = "10.0.1.202"
+SUBPort = 2345
+OCUPort = 1234
+RECV_BUFFER = 1024
+
+battery_pin = "P9_40"
+
+depth_pin = "P9_38"
+
+stepper1 = {"clk":"P8_17","dir":"P8_15"}
+
+stepper2 = {"clk":"P8_16", "dir":"P8_18"}
