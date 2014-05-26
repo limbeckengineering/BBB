d0420d2d9df4ccc89a02e57dac91b82af30f85e6
Limbeck Engineering LLC <limbeck.engineering@gmail.com>
2014-05-26 00:07:05 -0400

    Update BBstepper.py

--- float/BBstepper.py
+++ float/BBstepper.py
@@ -1,3 +1,5 @@
+#!/bin/env/python
+
 #enter stepper.'rotation'(pinarray, degrees, rpm)
 #1.8 per step
 
@@ -5,38 +7,54 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 import math
 import config
+import multiprocessing
 
-def step_forward(pins, pin_index):
-	GPIO.output(config.stepper1["clk"], GPIO.LOW)
-def step_backward(pins, pin_index):
-	GPIO.output(config.stepper1["clk"], GPIO.LOW)
-
-
-class Stepper(object):
-
-	def init_pins(self, pins):
-		GPIO.setup(config.stepper1["clk"], GPIO.OUT)
-		GPIO.setup(config.stepper1["dir"], GPIO.OUT)
-		GPIO.output(config.stepper1["clk"], GPIO.HIGH)
-		
-	def spin_clockwise(self, pins, rotations, rpm):
+def step_forward(pins, rotations, rpm):
+		from BBstepper import Stepper
+		stepper = Stepper()
 		sleep_time=0.3/float(rpm)
 		steps_forward = rotations*200
 		GPIO.output(config.stepper1["dir"], GPIO.HIGH)	
 		for x in range(0, steps_forward):
-			step_forward(self,pins)
+			GPIO.output(config.stepper1["clk"], GPIO.LOW)
 			time.sleep(sleep_time)
 			GPIO.output(config.stepper1["clk"], GPIO.HIGH)
-	
-	def spin_counterclockwise(self, pins, rotations, rpm):
+		stepper.spin_clockwise(pins, rotations, rpm, 0)
+
+def step_backward(pins, rotations, rpm):
+		from BBstepper import Stepper
+		stepper = Stepper()
 		sleep_time=.3/float(rpm)
 		steps_backward = rotations*200
 		GPIO.output(config.stepper1["dir"], GPIO.LOW)
 		for x in range(0, steps_backward):
-			step_backward(self,pins)
+			GPIO.output(config.stepper1["clk"], GPIO.LOW)
 			time.sleep(sleep_time)
 			GPIO.output(config.stepper1["clk"], GPIO.HIGH)
 		
-	def cleanup(self, pins):
+
+
+
+class Stepper(object):
+
+	def init_pins(self, pinarray):
+		GPIO.setup(pinaray["clk"], GPIO.OUT)
+		GPIO.setup(pinarray["dir"], GPIO.OUT)
+		GPIO.output(pinarray["clk"], GPIO.HIGH)
+		
+	def spin_clockwise(self, pinarray, rotations, rpm, signal):
+		clockwise = multiprocessing.Process(name='Clockwise', target=step_forward, args=(pins, rotations, rpm,,))
+		clockwise.start()
+		if signal == 0:
+			clockwise.terminate()
+		
+	
+	def spin_counterclockwise(self, pinarray, rotations, rpm, signal):
+		counterclock = multiprocessing.Process(name='CounterClock', target = step_backward, args=(pins, rotations,rpm,))
+		counterclock.start()
+		if signal == 0:
+			counterclock.terminate()
+		
+	def cleanup(self, pinarray):
 		GPIO.cleanup()		
 
