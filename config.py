66dc946b67c13453d3fa55bfdb5dd6d4abbb0e0b
limbeckengineering <limbeck.engineering@gmail.com>
2014-05-31 18:07:02 +0000

    added config

--- config.py
+++ config.py
@@ -2,12 +2,21 @@
 HOST = "10.0.1.202"
 SUBPort = 2345
 OCUPort = 1234
-RECV_BUFFER = 1024
+RECV_BUFFER = 2048
+
+
+Rasp1Port = 1244
+Rasp2Port = 1255
+RPi1HOST = "10.0.1.201"
+RPi2HOST = "10.0.1.201"
+
+beagleHost = "10.0.1.119"
+beaglePort = 2345
 
 battery_pin = "P9_40"
 
 depth_pin = "P9_38"
 
-stepper1 = {"clk":"P8_17","dir":"P8_15"}
+stepper1 = {"clk":"P8_11","dir":"P8_12"}
 
-stepper2 = {"clk":"P8_16", "dir":"P8_18"}
+stepper2 = {"clk":"P8_17", "dir":"P8_18"}
